fbaf3174dd230e87849cca709cbb5e8a3e324ca6
marcella-maia <marcellammchado@gmail.com>
2026-04-13 23:37:21 -0300

    feat(p1): implementa componente principal

--- projeto-investimentos/src/App.jsx
+++ projeto-investimentos/src/App.jsx
@@ -1,11 +1,20 @@
 const App = () => {
   return (
     <div className="container">
+      
       <div className="row">
-        <div className="col-12 text-center">
+        <div className="col-12 text-center mt-5">
           <h1>Hello, Investimentos</h1>
         </div>
       </div>
+
+      <div className="row mt-4">
+        <div className="col-12 text-start">
+          <h4>Simulador de Investimentos</h4>
+          <p>Descubra quanto seu dinheiro pode render com juros compostos</p>
+        </div>
+      </div>
+
     </div>
   )
 }
