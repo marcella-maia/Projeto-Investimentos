de96cd715e33b784707f82065d249d2de0abd20f
marcella-maia <marcellammchado@gmail.com>
2026-04-13 23:11:51 -0300

    feat(p1): implementa componente principal

---
+++ projeto-investimentos/src/App.jsx
@@ -0,0 +1,13 @@
+const App = () => {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-12 text-center">
+          <h1>Hello, Investimentos</h1>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
